7912c9051f30f799e2a5bac5bfb2e894bee75269
Abel Paul George <abel.paul.george@gmail.com>
2017-09-19 10:37:13 -0400

    HackGTeeny iOS Workshop
    Another simple app that was created to understand the early concepts of
    Swift and UIKit

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var aboutMeButton: UIButton!
+    
+    var pressed:Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        profileImageView.layer.cornerRadius = profileImageView.frame.height / 2
+        aboutMeButton.layer.cornerRadius = 10
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        pressed = !pressed
+        if pressed {
+            self.view.backgroundColor = UIColor.blue
+        } else {
+            self.view.backgroundColor = UIColor.white
+        }
+     
+    }
+    
 }
 
